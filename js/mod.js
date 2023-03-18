243dcfe47a91074f1ec59ca5141e716a0a28b66e
aguywhosaguy <caseypeg8@gmail.com>
2023-03-17 21:29:50 -0500

    D
    D

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Core Tree",
+	id: "coretree",
+	author: "aguywhosaguy",
+	pointsName: "cores",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
